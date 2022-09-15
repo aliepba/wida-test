4d61128e478b1eb9960c1f537871d6331d222f3d
Alief Bagaskara <43233348+aliepba@users.noreply.github.com>
2022-09-15 20:36:03 +0700

    Update index.js

--- test-invoice/controllers/ImportExcel/index.js
+++ test-invoice/controllers/ImportExcel/index.js
@@ -1,9 +1,5 @@
 const ImportExcel =  require('./import')
-const uploadExcel = require('./upload')
-const combi = require('./combination')
 
 module.exports = {
-    ImportExcel,
-    uploadExcel,
-    combi
+    ImportExcel
 }
