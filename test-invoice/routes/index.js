1eac71395f196c0a3dcf7d7e27d666dc6ba49af6
Alief Bagaskara <43233348+aliepba@users.noreply.github.com>
2022-09-15 20:37:03 +0700

    Update index.js

--- test-invoice/routes/index.js
+++ test-invoice/routes/index.js
@@ -4,13 +4,6 @@ var router = express.Router();
 const ImportController = require('../controllers/ImportExcel')
 const upload = require('../middleware/upload')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.post('/upload', upload.single('file') ,ImportController.uploadExcel);
 router.post('/excel', upload.single('file') ,ImportController.ImportExcel);
-router.get('/combi', ImportController.combi)
 
 module.exports = router;
